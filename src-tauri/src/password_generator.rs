484c2d5261a3622f947caeaf420e9e794de170c4
mszostak <01159105@pw.edu.pl>
2023-10-03 13:27:27 +0200

    passwords crate

--- src-tauri/src/password_generator.rs
+++ src-tauri/src/password_generator.rs
@@ -1,4 +1,7 @@
 
+// TODO: look into https://crates.io/crates/passwords
+// this crate has almost everything
+// also look at scoring
 #[tauri::command]
 pub fn generate_random_password() -> String {
     return "".to_string();
