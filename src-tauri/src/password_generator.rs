30db1476e7c2bc1d0383e7f0f9d66a2753c012bf
mszostak <01159105@pw.edu.pl>
2023-10-03 13:42:10 +0200

    password generation

--- src-tauri/src/password_generator.rs
+++ src-tauri/src/password_generator.rs
@@ -2,9 +2,33 @@
 // TODO: look into https://crates.io/crates/passwords
 // this crate has almost everything
 // also look at scoring
+
+use passwords::PasswordGenerator;
+
+
 #[tauri::command]
-pub fn generate_random_password() -> String {
-    return "".to_string();
+pub fn generate_random_password(
+    password_length: usize,
+    password_numbers: bool,
+    password_lowercase: bool,
+    password_uppercase: bool,
+    password_symbols: bool,
+    password_spaces: bool,
+    password_similar: bool
+) -> String {
+    let pg = PasswordGenerator {
+        length: password_length,
+        numbers: password_numbers,
+        lowercase_letters: password_lowercase,
+        uppercase_letters: password_uppercase,
+        symbols: password_symbols,
+        spaces: password_spaces,
+        exclude_similar_characters: password_similar,
+        strict: true, // leave it to true
+    };
+    let generated: Result<String, &str> = pg.generate_one();
+    // TODO: check strength here
+    return generated.unwrap().to_string();
 }
 
 #[tauri::command]
