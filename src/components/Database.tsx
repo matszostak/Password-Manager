ab33c46f985c9d0130f4bd2b79f608f7619da7c3
mszostak <01159105@pw.edu.pl>
2023-12-25 00:16:57 +0100

    a true Christmas Miracle, everything (almost) works

--- src/components/Database.tsx
+++ src/components/Database.tsx
@@ -1,25 +1,77 @@
-import { Box, Group, Button, Collapse, Table, Drawer, TextInput, PasswordInput, ActionIcon, Textarea, Text, Tooltip, Divider, Accordion, Center } from "@mantine/core"
+import { Box, Group, Button, Collapse, Table, Drawer, TextInput, PasswordInput, ActionIcon, Textarea, Text, Tooltip, Divider, Accordion, Center, keys, UnstyledButton, rem, ScrollArea } from "@mantine/core"
 import { useDisclosure } from "@mantine/hooks"
-import { IconDots, IconEye, IconEyeOff, IconRefresh, IconSettings } from "@tabler/icons-react"
+import { IconChevronDown, IconChevronUp, IconDots, IconEye, IconEyeOff, IconRefresh, IconSearch, IconSelector, IconSettings } from "@tabler/icons-react"
 import { useState } from "react"
 import { generatePassphrase } from "../utils/passwordGeneration"
 import PasswordGenerator from "./PasswordGenerator"
 
-interface AccordionLabelProps {
-    name: string;
-    urls: string;
+interface ThProps {
+    children: React.ReactNode;
+    reversed: boolean;
+    sorted: boolean;
+    onSort(): void;
 }
 
-function AccordionLabel({ name, urls }: AccordionLabelProps) {
+interface RowData {
+    name: string,
+    id: string,
+    username: string,
+    password: string,
+    urls: string[],
+    notes: string,
+}
+
+interface ThProps {
+    children: React.ReactNode;
+    reversed: boolean;
+    sorted: boolean;
+    onSort(): void;
+}
+
+function Th({ children, reversed, sorted, onSort }: ThProps) {
+    const Icon = sorted ? (reversed ? IconChevronUp : IconChevronDown) : IconSelector;
     return (
-        <Group wrap="nowrap">
-            <div>
-                <Text>{name}</Text>
-                <Text size="sm" c="dimmed" fw={400}>
-                    {urls}
-                </Text>
-            </div>
-        </Group>
+        <Table.Th>
+            <UnstyledButton onClick={onSort}>
+                <Group justify="space-between">
+                    <Text fw={500} fz="sm">
+                        {children}
+                    </Text>
+                    <Center>
+                        <Icon style={{ width: rem(16), height: rem(16) }} stroke={1.5} />
+                    </Center>
+                </Group>
+            </UnstyledButton>
+        </Table.Th>
+    );
+}
+
+function filterData(data: RowData[], search: string) {
+    const query = search.toLowerCase().trim();
+    return data.filter((item) =>
+        keys(data[0]).some((key) => String(item[key]).toLowerCase().includes(query))
+    );
+}
+
+function sortData(
+    data: RowData[],
+    payload: { sortBy: keyof RowData | null; reversed: boolean; search: string }
+) {
+    const { sortBy } = payload;
+
+    if (!sortBy) {
+        return filterData(data, payload.search);
+    }
+
+    return filterData(
+        [...data].sort((a, b) => {
+            if (payload.reversed) {
+                return String(b[sortBy]).localeCompare(String(a[sortBy]));
+            }
+
+            return String(a[sortBy]).localeCompare(String(b[sortBy]));
+        }),
+        payload.search
     );
 }
 
@@ -27,79 +79,78 @@ export default function Database({ parentState, setParentState }: { parentState:
     let parsedContent = JSON.parse(String(localStorage.getItem('dbContent')))
     let [parsedContentState, setParsedContentState] = useState(parsedContent)
     let name: string = parsedContentState.vaultName // database name from JSON
-    let creationDate: string = parsedContentState.creationdate // database creationdate from JSON
-    let dbVault = parsedContentState.vault // database vault
+    let dbVault = parsedContentState.vault
+
+    console.log('========================')
+    console.log(parsedContentState)
+    console.log('========================')
+
     const [opened, { open, close }] = useDisclosure(false);
     const [renameDrawer, renameDrawerHandler] = useDisclosure(false);
     const [collapse, collapseHandlers] = useDisclosure(false);
     const [generatedPassword, setGeneratedPassword] = useState<string | null>('')
     const [currentEntry, setCurrentEntry] = useState<any>()
-    const [itemStates, setItemStates] = useState(Array(dbVault.length).fill(false));
-    // Function to toggle the state of an item at a specific index
-    const toggleItemState = (index: number) => {
-        setItemStates((prevStates) =>
-            prevStates.map((state, i) => (i === index ? !state : state))
-        );
-    };
+    const [currentEntryIndex, setCurrentEntryIndex] = useState(-1)
 
     const handlePasswordSettingFunction = (passwordFromTheGenerator: string) => {
         setGeneratedPassword(passwordFromTheGenerator)
     }
 
+    const [search, setSearch] = useState('');
+    const [sortedData, setSortedData] = useState(dbVault);
+    const [sortBy, setSortBy] = useState<keyof RowData | null>(null);
+    const [reverseSortDirection, setReverseSortDirection] = useState(false);
+
+    const setSorting = (field: keyof RowData) => {
+        const reversed = field === sortBy ? !reverseSortDirection : false;
+        setReverseSortDirection(reversed);
+        setSortBy(field);
+        setSortedData(sortData(JSON.parse(JSON.stringify(parsedContentState.vault)), { sortBy: field, reversed, search }));
+    };
+
+    const handleSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const { value } = event.currentTarget;
+        setSearch(value);
+        setSortedData(sortData(JSON.parse(JSON.stringify(parsedContentState.vault)), { sortBy, reversed: reverseSortDirection, search: value }));
+    };
+
     const [currentUsername, setCurrentUsername] = useState('')
-    const map_password = dbVault.map(
-        (entry: any, index: number) =>
-            <Table.Tr key={entry.id} onClick={() => toggleItemState(index)}>
-                <Table.Td>{entry.username}</Table.Td>
-                <Table.Td>
-                    <PasswordInput
-                        variant="unstyled"
-                        value={entry.password}
-                        onChange={() => { }} // onChange to supress a warning
-                        w={160}
-                    />
-                </Table.Td>
-                <Table.Td>
-                    <Text truncate="end" w={160}>
-                        {entry.urls}
-                    </Text>
-                </Table.Td>
-                <Table.Td>
-                    <Button.Group>
-                        <Button color="indigo" w={80} onClick={() => {
-                            setCurrentEntry(entry) // TODO: create a state for every editable variable; then use those instead of a currentEntry
-                            setCurrentUsername(entry.username)
-                            renameDrawerHandler.open()
-                        }}>Edit</Button>
-                        <Button color="red" w={80}>Delete</Button>
-                    </Button.Group>
-                </Table.Td>
 
-            </Table.Tr>
-    )
+    const rows = sortedData.map((row: any, index: number) => (
+        <>
+            <Table.Tr >
 
-    const items = dbVault.map((item: any, index: number) => (
-        <Accordion.Item
-            key={item.id}
-            value={item.username}
-        >
-            <Center>
-                <Accordion.Control>
-                    <AccordionLabel {...item} />
-                </Accordion.Control>
-                <ActionIcon size="lg" variant="subtle" color="gray" onClick={() => console.log(item.username)}>
-                    <IconDots size="1rem" />
-                </ActionIcon>
-                <ActionIcon size="lg" variant="subtle" color="gray" onClick={() => {
-                            setCurrentEntry(item) // TODO: create a state for every editable variable; then use those instead of a currentEntry
-                            setCurrentUsername(item.username)
+                <Accordion.Item
+                    key={row.name}
+                    value={row.username}
+                    w={"300%"}
+                >
+                    <Center>
+                        <Accordion.Control>
+
+                            <Group wrap="nowrap">
+                                <div>
+                                    <Text>{row.name}</Text>
+                                    <Text size="sm" c="dimmed" fw={400}>
+                                        {row.urls}
+                                    </Text>
+
+                                </div>
+                            </Group>
+
+                        </Accordion.Control>
+                        <ActionIcon size="lg" variant="subtle" color="gray" onClick={() => {
                             renameDrawerHandler.open()
+                            setCurrentEntry(row)
+                            setCurrentEntryIndex(index)
                         }}>
-                    <IconDots size="1rem" />
-                </ActionIcon>
-            </Center>
-            <Accordion.Panel>{item.username + ' ' + item.password}</Accordion.Panel>
-        </Accordion.Item>
+                            <IconDots size="1rem" />
+                        </ActionIcon>
+                    </Center>
+                    <Accordion.Panel>{row.username + ' ' + row.password + ' ' + row.urls}</Accordion.Panel>
+                </Accordion.Item>
+            </Table.Tr>
+        </>
     ));
 
     async function generatePassphraseInterface(length: number, numbers: boolean, specialCharacter: string) {
@@ -219,11 +270,11 @@ export default function Database({ parentState, setParentState }: { parentState:
                             />
                             <Button
                                 onClick={() => {
-                                    let temp = currentEntry
-                                    temp.username = currentUsername
-                                    setCurrentEntry(temp)
+                                    let temp = parsedContentState
+                                    temp.vault[currentEntryIndex].username = currentUsername
+                                    setCurrentUsername('')
+                                    setParsedContentState(temp)
                                     renameDrawerHandler.close()
-                                    console.log(currentEntry)
                                     console.log(parsedContentState) // somehow the parsedContentState gets updated when currentEntry is updated???? but that is kind of good because it will be saved in the end
                                 }}
                             >
@@ -237,15 +288,63 @@ export default function Database({ parentState, setParentState }: { parentState:
                 <Button onClick={open}>Create new</Button>
             </Box>
 
-            <Accordion
-                variant="contained"
-                radius="md"
-                chevronPosition="left"
-                defaultValue={[]}
-                multiple={true}
-            >
-                {items}
-            </Accordion>
+            <TextInput
+                placeholder="Search by any field"
+                mb="md"
+                leftSection={<IconSearch style={{ width: rem(16), height: rem(16) }} stroke={1.5} />}
+                value={search}
+                onChange={handleSearchChange}
+            />
+            <ScrollArea>
+                <Accordion
+                    variant="contained"
+                    radius="md"
+                    chevronPosition="left"
+                    defaultValue={[]}
+                    multiple={true}
+                >
+                    <Table horizontalSpacing="md" verticalSpacing="xs" layout="fixed">
+                        <Table.Tbody>
+                            <Table.Tr>
+                                <Th
+                                    sorted={sortBy === 'name'}
+                                    reversed={reverseSortDirection}
+                                    onSort={() => setSorting('name')}
+                                >
+                                    Name
+                                </Th>
+                                <Th
+                                    sorted={sortBy === 'username'}
+                                    reversed={reverseSortDirection}
+                                    onSort={() => setSorting('username')}
+                                >
+                                    Username
+                                </Th>
+                                <Th
+                                    sorted={sortBy === 'urls'}
+                                    reversed={reverseSortDirection}
+                                    onSort={() => setSorting('urls')}
+                                >
+                                    Urls
+                                </Th>
+                            </Table.Tr>
+                        </Table.Tbody>
+                        <Table.Tbody>
+                            {rows.length > 0 ? (
+                                rows
+                            ) : (
+                                <Table.Tr>
+                                    <Table.Td colSpan={Object.keys(dbVault[0]).length}>
+                                        <Text fw={500} ta="center">
+                                            Nothing found
+                                        </Text>
+                                    </Table.Td>
+                                </Table.Tr>
+                            )}
+                        </Table.Tbody>
+                    </Table>
+                </Accordion>
+            </ScrollArea>
         </>
         // TODO: handle change and saving the database here
     );
