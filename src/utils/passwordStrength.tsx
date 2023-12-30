722b8995f7ba7cdb0896ed21da87a3b7a9934b0d
mszostak <01159105@pw.edu.pl>
2023-12-30 23:50:31 +0100

    fixed password strength multiplier

--- src/utils/passwordStrength.tsx
+++ src/utils/passwordStrength.tsx
@@ -27,9 +27,13 @@ export const requirements = [
 ];
 
 export function getStrength(password: string) {
-	let multiplier = password.length > 5 ? 0 : 1;
+	let multiplier = password.length > 15 ? 0 : 1;
 
-	
+  requirements.forEach((requirement) => {
+    if (!requirement.re.test(password)) {
+      multiplier += 1;
+    }
+  });
 
 	return Math.max(100 - (100 / (requirements.length + 1)) * multiplier, 10);
 }
